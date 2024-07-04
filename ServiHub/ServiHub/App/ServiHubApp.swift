62807f90d1ad936739fd6a5ce6eb606a2e80e709
Alejandro Alberto Gavira García <alex.gavira@gmail.com>
2024-07-04 19:31:36 +0200

    Version inicial app. 0.0

---
+++ ServiHub/ServiHub/App/ServiHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  ServiHubApp.swift
+//  ServiHub
+//
+//  Created by Alejandro Alberto Gavira García on 4/7/24.
+//
+
+import SwiftUI
+
+@main
+struct ServiHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
